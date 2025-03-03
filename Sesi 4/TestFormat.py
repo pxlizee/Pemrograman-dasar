5832ac4725cc3c9da0c40c3fa136e60c378d0350
pxlizee <pix.xiee666@gmail.com>
2025-03-03 13:38:56 +0700

    test

---
+++ Sesi 4/TestFormat.py
@@ -0,0 +1,5 @@
+hello_template = "halo, nama saya %s , saya berusia %d tahun, dan saya tinggal di %s"
+print(hello_template % ("ikbal hensem", 20, "Jakarta"))
+
+template_2 = "halo, nama saya {nama}, saya berusia {umur} tahun, dan saya tinggal di {alamat}"
+print(template_2.format(nama="ikbal hensem", umur=20, alamat="cianjur"))
