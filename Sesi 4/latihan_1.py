d0b26b4ace001f8d83b6c5bcdb1ce867572c0b00
pxlizee <pix.xiee666@gmail.com>
2025-03-03 15:12:38 +0700

    pause

---
+++ Sesi 4/latihan_1.py
@@ -0,0 +1,18 @@
+#buatlah aplikasi yang dapat menghitung tahun kabisat (tahun yang bisa di bagi 4)
+#buatlah untuk menentukan ouput angka negatif atau positif
+#aplikasi untuk menghitung diskon, menghitung total yang harus dibayar berdasarkan diskon akhir tahun 
+
+
+#1
+tahun = input('masukkan tahun ')
+
+if int(tahun) % 4 == 0:
+    print("ini adalah tahun kabisat")
+else:
+    (print('ini bukan tahun kabisat'))
+    
+
+
+ 
+
+
---
+++ Sesi 4/latihan_2.py
@@ -0,0 +1,8 @@
+angka = input("Masukkan Angka :")
+
+if int(angka) == 0 :
+    print("angka 0")
+elif int(angka) >= 0 :
+    print("angka positif")
+else :
+    print("angka negatif")
