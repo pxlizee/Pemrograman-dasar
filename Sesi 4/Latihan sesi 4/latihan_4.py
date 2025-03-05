1bd45e1bbcf9e5b3434bce108c916e9957c4a692
pxlizee <pix.xiee666@gmail.com>
2025-03-06 00:46:51 +0700

    doneee

--- Sesi 4/Latihan sesi 4/latihan_4.py
+++ Sesi 4/Latihan sesi 4/latihan_4.py
@@ -8,4 +8,20 @@ nilai = (nilai_ing, nilai_mtk, nilai_ipa)
 
 print(f"nilai rata rata seorang siswa dari 3 mata pelajaran adalah {rata_rata}")
 
-nilai_dibawah_70 = 
+nilai_dibawah_70 = 1
+if(nilai_ipa) :
+    nilai_dibawah_70 += 1
+if(nilai_ing) :
+    nilai_dibawah_70 += 1
+if(nilai_mtk) :
+    nilai_dibawah_70 += 1
+    
+if (rata_rata > 75) :
+    print("Siswa ini lulus, karena nilai rata rata nya di atas 75")
+elif (nilai.count(100) >= 1) :
+    print("Siswa ini lulus, karena ada nilai yang mencapai 100")
+elif (nilai_dibawah_70 == 1) :
+    print("siswa ini lulus, karena nilai di bawah 70 hanya 1")
+
+else :
+    print("tidak lulus, tidak memenuhi syarat")
