2d95b3f974701df38f516195656cb3631ffc9ac4
pxlizee <pix.xiee666@gmail.com>
2025-03-12 21:05:09 +0700

    doneeee

--- Sesi 5/Latihan sesi 5/Soal10.py
+++ Sesi 5/Latihan sesi 5/Soal10.py
@@ -1,40 +1,41 @@
-import random
+import random 
 
-
-print('permainan Batu Gunting Kertas')
-pemain = str(input('Batu / Gunting / Kertas?'))
-comp = str (random.choice(['Batu', 'Gunting', 'Kertas']))
-print('Pemain memilih: ', pemain)
-print('Komputer memilih: ', comp)
-
-
-
-if pemain == str('Batu') or pemain == str('Kertas') or pemain == str('Gunting') :
-    print('input Valid')
-else :
-    print('Input tidak valid')
+def main():
+    print('Batu Gunting Kertas Anjay')
     
-
-skor_P = 0
-skor_C = 0
-if pemain == comp:
-    skor_P= 0
-    print('Seri!')
-elif pemain == 'Batu' and comp == 'Gunting':
-    skor_P += 1
-    print('Pemain menang!')
-elif pemain == 'Gunting' and comp == 'Kertas' :
-    skor_P += 1
-    print('Pemain Menang')
-elif pemain == 'Kertas' and comp == 'Batu':
-    skor_P += 1
-    print('Pemain Menang')
-else :
-    skor_C += 1
-    print('Pemain Kalah')
+    skor_P = 0 
+    skor_C = 0 
+    continue_game = True
     
-
+    while continue_game:
+        pemain = input('Batu / Gunting / Kertas ? ').capitalize()
+        komputer = random.choice(['Batu', 'Gunting', 'Kertas'])
+        print ('Pemain Memilih :', pemain)
+        print("Komputer Memilih :", komputer)
+        
+        if pemain not in ['Batu', 'Gunting', 'Kertas'] :
+            print('Input Tidak Valid. Permainan Berakhir')
+            break
+        
+        if pemain == komputer:
+            print('Seri Boss')
+        elif pemain == 'Batu' and komputer == 'Gunting' or pemain == 'Gunting' and komputer == 'Kertas' or pemain == 'Kertas' and komputer == 'Batu' :
+            skor_P += 1
+            print('Pemain Menang')
+        else:
+            skor_C += 1 
+            print('Pemain Kalah !')
+            
+        print('Skor Pemain :', skor_P)
+        print('Skor Komputer :', skor_C)
+        
+        lanjut = input('Apakah Anda ingin Melanjutkan Permainan?? (Y/N)').lower()
+        if lanjut != 'y':
+            continue_game = False
+            
+    print('Skor Akhir Pemain :' , skor_P)
+    print('Skor Akhir Komputer: ', skor_C)
     
-print('Skor Pemain: ', skor_P)
-print('Skor Computer: ', skor_C)
-    
+ 
+if __name__ == "__main__" :
+    main()
