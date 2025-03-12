7c9e290134479fa02cb5408d9d44aa663ef342a8
pxlizee <pix.xiee666@gmail.com>
2025-03-12 20:37:48 +0700

    tinggal no 10

--- Sesi 5/Latihan sesi 5/Soal10.py
+++ Sesi 5/Latihan sesi 5/Soal10.py
@@ -1,2 +1,40 @@
+import random
+
+
 print('permainan Batu Gunting Kertas')
 pemain = str(input('Batu / Gunting / Kertas?'))
+comp = str (random.choice(['Batu', 'Gunting', 'Kertas']))
+print('Pemain memilih: ', pemain)
+print('Komputer memilih: ', comp)
+
+
+
+if pemain == str('Batu') or pemain == str('Kertas') or pemain == str('Gunting') :
+    print('input Valid')
+else :
+    print('Input tidak valid')
+    
+
+skor_P = 0
+skor_C = 0
+if pemain == comp:
+    skor_P= 0
+    print('Seri!')
+elif pemain == 'Batu' and comp == 'Gunting':
+    skor_P += 1
+    print('Pemain menang!')
+elif pemain == 'Gunting' and comp == 'Kertas' :
+    skor_P += 1
+    print('Pemain Menang')
+elif pemain == 'Kertas' and comp == 'Batu':
+    skor_P += 1
+    print('Pemain Menang')
+else :
+    skor_C += 1
+    print('Pemain Kalah')
+    
+
+    
+print('Skor Pemain: ', skor_P)
+print('Skor Computer: ', skor_C)
+    
