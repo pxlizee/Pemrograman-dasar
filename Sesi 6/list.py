068204251d6c6d820b2daad7ede0f76eb1c48f46
pxlizee <pix.xiee666@gmail.com>
2025-03-17 13:35:39 +0700

    looping

---
+++ Sesi 6/list.py
@@ -0,0 +1,4 @@
+motor = ["F1ZR", "RXZ", "Ninja RR", "NSR"]
+
+for name in motor :
+    print(name, "Adalah Motor 2tak")
---
+++ Sesi 6/loop_dasar.py
@@ -0,0 +1,7 @@
+for i in range(1000) :
+    print(i, "Ikbal love riki")
+    
+i = 0
+while (i <20):
+    print(i, "Ikbal love riki")
+    i += 1
