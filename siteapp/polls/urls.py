6cb36f9d7bb2c502f93f485d71e1c6410a8f66c6
pxlizee <pix.xiee666@gmail.com>
2025-06-21 22:28:35 +0700

    ssss

--- siteapp/polls/urls.py
+++ siteapp/polls/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name = 'polls'
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', views.index, name='index'),
+    path('int:question/<int:question_id>/', views.results, name='results'),
+    path('address/', views.address, name='address'),
+    path('phone/', views.phone, name='phone'),
 ]
--- siteapp/polls/views.py
+++ siteapp/polls/views.py
@@ -1,7 +1,33 @@
+from django.shortcuts import get_object_or_404, render
+from .models import Question, Choice
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ""
+    for q in latest_question_list:
+        output += f"{q.question_text}<br>"
+        # answer list
+        answer_list = Choice.objects.filter(question=q)
+        list_answer = ", ".join([a.choice_text for a in answer_list])
+        output += f"Pilihan: {list_answer}"
+        output += "<br><br>"
+    return HttpResponse(output)
+
+def html_index(request):
+   latest_question_list = Question.objects.order_by('-pub_date')[:5]
+   answer_list = Choice.objects.all()
+   context = { "latest_question_list": latest_question_list,
+       "answer_list": answer_list
+   }
+   return render(request, "index.html", context)
+
+
+
+def results(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
 def address(requset):
     html_content = """
     <!DOCTYPE html>
